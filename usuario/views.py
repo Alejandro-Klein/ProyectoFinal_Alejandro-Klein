6a0cf74c09683baa9848ac1509d4e6d07fed425d
Alejandro Klein <aleklein2014@gmail.com>
2023-07-15 22:52:52 -0300

    Se agrega imagen y modificacion de usuario

--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm , UserCreationForm
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import authenticate, login as django_login
-from usuario.form import MiFormularioDeCreacionDeUsuarios
+from usuario.form import MiFormularioDeCreacionDeUsuarios,MiFormularioDeEdicionDeDatosDeUsuario
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -38,3 +42,25 @@ def registrarse(request):
     
     formulario = MiFormularioDeCreacionDeUsuarios()
     return render(request, 'usuario/registro.html',{'formulario': formulario})
+
+@login_required
+def edicion_perfil(request):
+    
+    if request.method == 'POST':
+        formulario = MiFormularioDeEdicionDeDatosDeUsuario(request.POST, instance=request.user)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('inicio:inicio')
+        else:
+            return render(request, 'usuario/edicio_perfil.html',{'formulario': formulario})
+    
+    formulario = MiFormularioDeEdicionDeDatosDeUsuario(instance=request.user)
+    return render(request,'usuario/edicion_perfil.html',{'formulario':formulario})
+
+class ModificarPass(LoginRequiredMixin,PasswordChangeView):
+    template_name = 'usuario/modificar_pass.html'
+    success_url = reverse_lazy('usuario:editar_perfil')
+    
+
+
+
